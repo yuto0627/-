d4305ab3a3c82fe485614488b0e344858b3a07e3
yutoishida <yuto.ishida@seattleconsulting.co.jp>
2020-04-09 17:25:06 +0900

    【ポートフォリオ実装】20200409

---
+++ src/components/About.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="aboutSection">
+        <div id="aboutTitle">About Me</div>
+        <div id="aboutExplain">あああああ</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'About'
+}
+</script>
+
+<style scoped>
+
+#aboutSection {
+    background-color: #F5F5F5;
+    width: 100%;
+    height: auto;
+    text-align: center;
+}
+
+#aboutTitle {
+    color: #20879F;
+    font: Noto Sans ;
+    font-weight: bold;
+    font-size: 18px;
+    text-shadow: 2px 2px 6px;
+    text-align: center;
+}
+
+#aboutExplain {
+    color: #707070;
+    font: Noto Sans;
+    font-size: 12pt;
+    word-break: break-all;
+    line-height: 1.5em;
+}
+
+</style>
